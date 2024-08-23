6d72d5e2e2234870cdbd632e5b983714852f1d35
Pulkit Thakar <pulkit.thakar@gmail.com>
2024-08-23 13:06:42 -0500

    Made a homepage component; Set background image for the hero page within homeapage;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @Component({
   selector: 'MahCol-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HomePageComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
